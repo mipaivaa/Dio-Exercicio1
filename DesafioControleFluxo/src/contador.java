bbd91dec8dc843faa6375cdf17a28ddc01025f6d
Michelle Paiva <120685216+mipaivaa@users.noreply.github.com>
2024-05-15 13:57:36 -0300

    Add files via upload

---
+++ DesafioControleFluxo/src/contador.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Por favor Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Por favor Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Conclusão> Imprimindo o número " + i);
+        }
+    }
+}
